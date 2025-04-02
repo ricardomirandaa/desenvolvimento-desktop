8b39fdb2d5560b59305d9d55b0dd07c2a6908460
ricardo <riccardoomirandaa@gmail.com>
2025-04-01 21:38:12 -0300

    feat: adiciona script de banco de dados para as tabelas de: categorias e produtos

---
+++ MultApps/MODELS/MultApps.Models/Entities/Abstract/EntidadeBase.cs
@@ -0,0 +1,17 @@
+﻿using MultApps.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultApps.Models.Entities.Abstract
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
---
+++ MultApps/MODELS/MultApps.Models/Entities/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultApps.Models.Entities.Abstract
+{
+    internal class Categoria : EntidadeBase
+    {
+        public string Nome { get; set; }
+    }
+}
